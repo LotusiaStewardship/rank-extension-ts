5f9f9325ca4bc32d6c91a14569145d14ec2e17d2
maff1989 <matthew@urgero.net>
2024-11-27 05:14:55 -0600

    Improve DOM update logic, add `hidePost` function
    Previously there were dozens of redundant processing cycles. This commit
    processes specific DOM element updates and parses each one individually
    rather than parsing an entire collection at once. Now, only each new
    tweet element that is added into the DOM is processed accordingly.
    
    Testing is being performed with an unauthenticated incognito session on
    a user's profile page. The observed behavior removes and adds tweet
    elements into the DOM during page scroll. With the new logic, each
    tweet is processed as Twitter's logic adds it into the DOM.
    
    We also add a first implementation of `hidePost`, which adds a custom
    class to tweets that exceed the negative threshold. It seems the
    `display: none;` is not being applied (blocked by browser?); will need
    to figure this out.

--- wxt/entrypoints/twitter.content/index.ts
+++ wxt/entrypoints/twitter.content/index.ts
@@ -1,112 +1,143 @@
-import { Parser } from "@/utils/parser";
-import { Selector } from "@/utils/selector";
-import assert from "assert";
-import { load } from "cheerio";
+import { Parser } from '@/utils/parser'
+import { Selector } from '@/utils/selector'
+import { load } from 'cheerio'
+import { RANK_API } from '@/utils/constants'
+import './style.css'
 
-const posts = [
-  {"platform":"twitter","profileId":"seanballard","postId":"1802810866061988228","ranking":"-62669000000","votesPositive":0,"votesNegative":62669}
+type Post = {
+  platform: string;
+  profileId: string;
+  postId: string;
+  ranking: bigint;
+  votesPositive: number;
+  votesNegative: number;
+}
+const posts: Post[] = [
+  {
+    platform: 'twitter',
+    profileId: 'seanballard',
+    postId: '1585519088238993410',
+    ranking: -62669000000n,
+    votesPositive: 0,
+    votesNegative: 62669,
+  },
 ]
+const observers: Map<'root' | 'timeline', MutationObserver> = new Map()
+
 
 export default defineContentScript({
   matches: ['*://*.x.com/*', '*://x.com/*'],
-  runAt: 'document_end', // https://developer.chrome.com/docs/extensions/reference/api/extensionTypes#type-RunAt
+  // https://developer.chrome.com/docs/extensions/reference/api/extensionTypes#type-RunAt
+  world: 'ISOLATED',
+  runAt: 'document_end',
   async main() {
-    // 
-    const timelineObserver = new MutationObserver(timelineObserve)
+    // Query the root container for observation
     const root = document.body.querySelector(Selector.TwitterContainer.div.root)
-    const rootObserver = new MutationObserver(() => {
+    // Set up root container observer and callback
+    observers.set('root', new MutationObserver(() => {
       // Query the timeline div
-      const timeline = document.body.querySelector(
-        Selector.TwitterContainer.div.timeline
-      )
+      const timeline = document.body.querySelector(Selector.TwitterContainer.div.timeline)
       // Disconnect root observer and set up timeline observer if found
       if (timeline) {
-        timelineObserver.observe(timeline, {
-          childList: true, subtree: true
+        observers.get('timeline')?.observe(timeline, {
+          childList: true,
+          subtree: true,
         })
-        rootObserver.disconnect()
+        observers.get('root')?.disconnect()
+        observers.delete('root')
       }
-    })
+    }))
+    // Instantiate timeline observer
+    observers.set('timeline', new MutationObserver(timelineObserve))
     // Begin observing root node for DOM updates (mutations)
-    rootObserver.observe(root as Node, {
+    observers.get('root')?.observe(root as Node, {
       childList: true,
       subtree: true,
-      attributes: true
+      attributes: true,
     })
-
-    try {
-      const profile = await fetch('https://rank.lotusia.org/api/v1/twitter/owenbenjamin')
-      const json = await profile.json()
-      console.log(json)
-    } catch (e) {
-      console.log(`fuck ${e}`)
-    }
   },
-});
-
-const rootObserve = (mutations: MutationRecord[]) => {
-  if (mutations.length > 0) {
-    mutations.forEach(mutation => {
-      console.log(`mutation ${mutation.target.textContent}`)
-    })
-    //const $ = load(html)
-    //const timeline = $(Selector.TwitterContainer.div.timeline)
-    //const $ = load(document.body.innerHTML)
-    //const timeline = $(Selector.TwitterContainer.div.timeline)
-    //console.log(`timeline ${timeline}`)
-    //if (timeline) {
-    //  //processTimeline(timeline)
-    //  // Start timeline observer
-    //  //timelineObserver.observe(timeline, {
-    //  //  childList: true, subtree: true
-    //  //})
-    //  // Stop redundant root observer
-    //  //rootObserver.disconnect()
-    //  // Process timeline
-    //  //processTimeline(timeline)
-    //}
-  }
-}
+})
 
 const datestr = () => new Date().toISOString()
-function timelineObserve(mutations: MutationRecord[]) {
+const timelineObserve = (mutations: MutationRecord[]) => {
   console.log(`timeline observed`)
-  mutations.forEach(mutation => {
-    const target = mutation.target as HTMLElement
-    const $ = load(target.outerHTML)
-    
-    const tweets = $(Selector.TwitterArticle.div.tweet)
-    console.log(`tweets ${tweets.length}`)
-    if (tweets.length > 0) {
-      for (const tweet of tweets) {
-        console.log(tweet)
+  mutations.forEach(async mutation => {
+    for (const node of mutation.addedNodes) {
+      console.log(node)
+      //console.log(`added node ${(node as HTMLElement).outerHTML}`)
+      const added = node as HTMLElement
+      const $ = load(added.outerHTML)
+      const tweet = $(Selector.TwitterArticle.div.tweet)
+      // only process elements with a single tweet; avoids duplicate processing
+      if (tweet.length == 1) {
         const t_s0 = performance.now()
+        const $ = load(tweet.html() as string)
+        // Select elements
+        const tweetTextDiv = $(Selector.TwitterArticle.div.tweetText)
+        const tweetUserNameLink = $(Selector.TwitterArticle.a.tweetUserName)
+        const postIdLink = $(Selector.TwitterArticle.a.tweetId)
+        // Parse elements for text data
+        const postText = Parser.TwitterArticle.postTextFromElement(tweetTextDiv)
+        const userName = Parser.TwitterArticle.userNameFromElement(tweetUserNameLink)
+        const postId = Parser.TwitterArticle.postIdFromElement(postIdLink)
+        // Verify parsed DOM data
         try {
-          // Select elements
-          const tweetTextDiv = $(Selector.TwitterArticle.div.tweetText, tweet)
-          const tweetUserNameLink = $(Selector.TwitterArticle.a.tweetUserName, tweet)
-          const postIdLink = $(Selector.TwitterArticle.a.tweetId, tweet)
-          // Parse elements for text data
-          const postText = Parser.TwitterArticle.postTextFromElement(tweetTextDiv)
-          const userName = Parser.TwitterArticle.userNameFromElement(tweetUserNameLink)
-          const postId = Parser.TwitterArticle.postIdFromElement(postIdLink)
-          // Verify text data
-          assert(postText, 'tweet text not found')
-          assert(userName, 'username not found')
-          assert(postId, 'post ID not found')
-          // fetch post from storage if available
-          storage.getItem(`local:${userName}::${postId}`)
-
+          console.assert(postText, 'tweet text not found')
+          console.assert(userName, 'username not found')
+          console.assert(postId, 'post ID not found')
+      
           const t_s1 = (performance.now() - t_s0).toFixed(3)
           console.log(`${datestr()}: ${userName}: ${postId}: ${postText} (${t_s1}ms)`)
         } catch (e) {
-
+          // issue parsing for required data; continue to next tweet
+          //console.log(e, html)
+          continue
+        }
+        // fetch tweet data from RANK API
+        try {
+          const profile = await fetch(
+            `${RANK_API}/twitter/${userName}/status/${postId}`,
+            {
+              mode: 'no-cors'
+            }
+          )
+          const json = await profile.json()
+          console.log(json)
+        } catch (e) {
+          // ignore; likely API does not have post indexed
+        }
+      
+        /**
+         * Won't need this
+         * We are now correctly processing new HTML nodes one at a time
+        // fetch post from storage if available
+        let storedPost: Post
+        try {
+          const post = await storage.getItem(`local:${userName}::${postId}`)
+          console.assert(post, `post ID not found in storage ${postId}`)
+          storedPost = post as Post
+        } catch (e) {
+          // add post if not found in local cache
+          await storage.setItem(`local:${userName}::${postId}`, posts[0])
+          storedPost = posts[0]
+        } 
+        */
+      
+        // apply ranking
+        if (posts[0].postId == postId) {
+          if (posts[0].ranking < 0n) {
+            console.log(`hiding tweet ID ${postId}`)
+            hidePost(postId)
+          }
         }
       }
     }
   })
-  //const timeline = document.querySelector(Selector.TwitterContainer.div.timeline)
-  //if (timeline) {
-  //  processTimeline(timeline)
-  //}
+}
+
+const hidePost = async (postId: string) => {
+  const selector = `${Selector.TwitterArticle.div.tweet}:has(a[href*="/status/${postId}"])`
+  const domElement = document.body.querySelector(selector)
+  domElement?.classList.add('hidden-downranked')
+  console.log(document.body.querySelector(selector))
 }
