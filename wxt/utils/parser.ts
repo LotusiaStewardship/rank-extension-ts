f359f1a9401ac3ec87341b53fd02d171dcb17ab1
maff1989 <matthew@urgero.net>
2024-12-06 10:13:17 -0700

    Twitter: correctly parse `postId`
    Previous behavior would always pull the postId as the last URI of the
    `/status` path, but sometimes there are additional URIs appended to the
    full URL path. This commit grabs the `postId` as the URI that follows
    the `/status` URI.

--- wxt/utils/parser.ts
+++ wxt/utils/parser.ts
@@ -12,7 +12,10 @@ export namespace Parser {
       postTextElement.children('span').first().text()
     public static userNameFromElement = (userNameLinkElement: Cheerio<Element>) =>
       userNameLinkElement.attr('href')?.split('/')?.pop()?.toLowerCase()
-    public static postIdFromElement = (postIdElement: Cheerio<Element>) =>
-      postIdElement.attr('href')?.split('/').pop()
+    public static postIdFromElement = (postIdElement: Cheerio<Element>) => {
+      const uriArray = postIdElement.attr('href')?.split('/')
+      const statusUriIndex = uriArray?.findIndex(uri => uri == 'status') as number
+      return uriArray && statusUriIndex ? uriArray[statusUriIndex + 1] : null
+    }
   }
 }
