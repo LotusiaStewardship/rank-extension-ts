0e1155a718ed3cbb3993f7c73ceca1f9050e66c1
maff1989 <matthew@urgero.net>
2024-12-14 15:24:09 -0700

    Fix Parser for `postId`, `userName` -> `profileId`
    Parsing for `postId` would grab the last chunk of the URL as the `postId`
    but this does not always work. Now the parser will find the `/status/`
    chunk and take the next chunk as the `postId`.

--- wxt/utils/parser.ts
+++ wxt/utils/parser.ts
@@ -10,12 +10,18 @@ export namespace Parser {
   export class TwitterArticle {
     public static postTextFromElement = (postTextElement: Cheerio<Element>) =>
       postTextElement.children('span').first().text()
-    public static userNameFromElement = (userNameLinkElement: Cheerio<Element>) =>
+    public static profileIdFromElement = (userNameLinkElement: Cheerio<Element>) =>
       userNameLinkElement.attr('href')?.split('/')?.pop()?.toLowerCase()
     public static postIdFromElement = (postIdElement: Cheerio<Element>) => {
       const uriArray = postIdElement.attr('href')?.split('/')
-      const statusUriIndex = uriArray?.findIndex(uri => uri == 'status') as number
-      return uriArray && statusUriIndex ? uriArray[statusUriIndex + 1] : null
+      if (!uriArray) {
+        return null
+      }
+      const statusUriIndex = uriArray.findIndex(uri => uri == 'status')
+      if (statusUriIndex < 0) {
+        return null
+      }
+      return uriArray[statusUriIndex + 1]
     }
   }
 }
