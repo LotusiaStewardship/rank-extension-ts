45f21c65f37aae0289d6cf2fa1379be500b04ec7
maff1989 <matthew@urgero.net>
2025-01-18 15:04:38 -0700

    Add default RANK `OP_RETURN` value
    Necessary for actually burning Lotus when crafting RANK transactions.
    Forgot to include this commit in #11

--- utils/constants.ts
+++ utils/constants.ts
@@ -5,3 +5,5 @@ export const DEFAULT_RANK_TTL = 10 // number of seconds profile/post ranking is 
 export const WALLET_CHRONIK_URL = 'https://chronik.lotusia.org'
 export const WALLET_BIP44_PURPOSE = 44
 export const WALLET_BIP44_COINTYPE = 10605
+// RANK tx constants
+export const RANK_OUTPUT_MIN_VALUE = 100_000_000
