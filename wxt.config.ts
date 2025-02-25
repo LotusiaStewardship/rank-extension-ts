a2725182c25a1c5f36cfb6cf2861296a64c6e9c2
Matthew 🪷 <matthew@urgero.net>
2025-02-24 20:51:57 -0600

    v0.3.2-alpha (#33)

--- wxt.config.ts
+++ wxt.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
     name: 'Lotus Voting System',
     description:
       'A decentralized, community-moderated reputation system for social media, powered by Lotus',
-    version: '0.3.1',
-    version_name: '0.3.1-alpha',
+    version: '0.3.2',
+    version_name: '0.3.2-alpha',
     host_permissions: ['*://rank.lotusia.org/api/v1/*', '*://chronik.lotusia.org/*'],
     permissions: ['storage', 'notifications'],
   },
