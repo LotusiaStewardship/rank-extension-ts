ef323cf3966d4cd2b9de1403ebe046e7c9720da3
maff1989 <matthew@urgero.net>
2025-04-17 05:54:17 -0700

    Function updates

--- utils/functions.ts
+++ utils/functions.ts
@@ -6,7 +6,8 @@ export const toXPI = (sats: string) =>
     minimumFractionDigits: 2,
     maximumFractionDigits: 6,
   })
-export const toMinifiedNumber = (number: number) => {
+export const toMinifiedNumber = (number: number, divisor: number = 1) => {
+  number = Math.floor(number / divisor)
   if (number > 1e9) {
     return `${(number / 1e9).toFixed(1)}B`
   } else if (number > 1e6) {
@@ -30,20 +31,22 @@ export const deserialize = (storeData: string) => {
 }
 export const newInstanceId = async (runtimeId: string) => {
   const difficulty = 1
+  const leadingZeroes = String().padStart(difficulty, '0')
   let nonce = 0
   let hash = ''
   let checkpointTime = Date.now()
   const startTime = Date.now()
+  //console.log('mining for instanceId at', new Date(startTime).toISOString())
   while (true) {
     const data = Buffer.from(`${runtimeId}:${startTime}:${nonce}`)
     const computed = await crypto.subtle.digest('SHA-256', data)
     hash = Buffer.from(computed).toString('hex')
-    if (hash.substring(0, difficulty) == '0') {
-      console.log('generated instance id', hash, nonce, startTime)
+    if (hash.substring(0, difficulty) == leadingZeroes) {
+      console.log('mined instance id', hash, nonce, startTime)
       break
     }
     const now = Date.now()
-    if (now - checkpointTime >= 10000) {
+    if (now - checkpointTime >= 10_000) {
       const hashrate = (nonce / ((now - startTime) / 1_000)).toFixed(3)
       console.log(`hashrate: ${hashrate}H/s`)
       checkpointTime = now
