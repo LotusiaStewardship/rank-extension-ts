6aca73d46ea5cc7633df5c19de9a2acef3c979c2
Matthew 🪷 <matthew@urgero.net>
2025-05-09 12:16:12 -0500

    v1.0.1-beta (#58)

--- wxt.config.ts
+++ wxt.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
     name: 'Lotusia',
     description:
       'A decentralized, community-moderated reputation system for social media, powered by Lotus',
-    version: '1.0.0',
-    version_name: '1.0.0-beta',
+    version: '1.0.1',
+    version_name: '1.0.1-beta',
     host_permissions: [
       '*://rank.lotusia.org/api/v1/*',
       '*://chronik.lotusia.org/*',
