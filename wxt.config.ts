56ddab0d4f7aa1f36c1a0b6dd1e419a1243b3363
maff1989 <matthew@urgero.net>
2025-09-20 16:12:54 -0700

    v1.2.4-beta
    Per title

--- wxt.config.ts
+++ wxt.config.ts
@@ -18,8 +18,8 @@ export default defineConfig({
     name: 'Lotusia',
     description:
       'A decentralized, community-moderated reputation system for social media, powered by Lotus',
-    version: '1.2.3',
-    version_name: '1.2.3-beta',
+    version: '1.2.4',
+    version_name: '1.2.4-beta',
     host_permissions: [
       '*://rank.lotusia.org/api/v1/*',
       '*://chronik.lotusia.org/*',
